5eecf9a3a0bd577c3663287910fd6321164d3947
soufiane fatih <fatihe.soufiane@gmail.com>
2024-01-09 16:42:24 +0100

    add soe changed

--- DAY_2/loops.js
+++ DAY_2/loops.js
@@ -2,29 +2,66 @@
 //  Write a program that calculates the factorial of a number: 
 
 
-
 let num = 5 ;
 let result = 1;
 
 for (let i = 1 ; i <= num; i++) {
-    result *= i;
-    console.log(result); 
+    result=result* i;
 }
 
 console.log(result); 
 
 
 
+// Task 2 : How many digits ?
+// Giving a number and using loops, write a program that give you how many digits in this number :
 
-// function calculateFactorial(n) {
-//     let result = 1;
-  
-//     for (let i = 1; i <= n; i++) {
-//       result *= i;
-//     }
-  
-//     return result;
-//   }
-  
-//   const factorialOf5 = calculateFactorial(5);
-//   console.log(factorialOf5); // Output: 120
+let number = 12345;
+let count = 1;
+  while (number >= 1) {
+    number = (number / 10); 
+    count++;
+    console.log(count);
+  }
+
+
+
+console.log(count);
+
+
+
+// Task 3 :  Time to draw !
+// Using loops draw the following tree: 
+
+// const n = 4;
+
+// for (let i = 1; i <= n; i++) {
+//   let spaces = ' '.repeat(n - i);
+//   let stars = '*'.repeat(2 * i - 1);
+//   console.log(spaces + stars);
+// }
+
+
+
+const n = 4;
+
+for (let i = 1; i <= n; i++) {
+  let spaces = '';
+  let stars = '';
+
+  // Add spaces
+  for (let j = 1; j <= n-i ; j++) {
+    spaces += ' ';
+    // console.log('how',j);
+    // console.log('spaces',spaces);
+  }
+  // Add stars
+  for (let k = 1; k <= 2 * i-1; k++) {
+    stars += '*';
+  }
+
+  console.log( spaces + stars );
+}
+
+
+
